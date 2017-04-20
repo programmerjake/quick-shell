766f28a22aeea454e03128df8c48331f68920a92
Jacob Lifshay <programmerjake@gmail.com>
2017-04-19 17:54:58 -0700

    switched to peg parser

---
+++ parser/parser.cpp
@@ -0,0 +1,300 @@
+// automatically generated from parser.peg
+#line 1 "parser.peg"
+              
+/*
+ * Copyright 2017 Jacob Lifshay
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#line 21 "parser.cpp"
+#include "parser.h"
+
+namespace quick_shell
+{
+namespace parser
+{
+Parser::Parser(std::shared_ptr<const char32_t> source, std::size_t sourceSize)
+    : resultsPointers(sourceSize, nullptr),
+      resultsChunks(),
+      eofResults(),
+      source(std::move(source)),
+      sourceSize(sourceSize)
+{
+}
+
+Parser::Parser(std::u32string source) : Parser(makeSource(std::move(source)))
+{
+}
+
+Parser::Parser(const char *source, std::size_t sourceSize) : Parser(makeSource(source, sourceSize))
+{
+}
+
+Parser::Parser(const char32_t *source, std::size_t sourceSize)
+    : Parser(makeSource(std::u32string(source, sourceSize)))
+{
+}
+
+std::pair<std::shared_ptr<const char32_t>, std::size_t> Parser::makeSource(std::u32string source)
+{
+    auto sourceSize = source.size();
+    auto pSource = std::make_shared<std::u32string>(std::move(source));
+    return std::make_pair(std::shared_ptr<const char32_t>(pSource, pSource->data()), sourceSize);
+}
+
+std::pair<std::shared_ptr<const char32_t>, std::size_t> Parser::makeSource(const char *source,
+                                                                           std::size_t sourceSize)
+{
+    std::u32string retval;
+    retval.reserve(sourceSize);
+    std::size_t position = 0;
+    const char32_t replacementChar = U'\uFFFD';
+    while(position < sourceSize)
+    {
+        unsigned long byte1 = source[position++];
+        if(byte1 < 0x80)
+        {
+            retval += static_cast<char32_t>(byte1);
+            continue;
+        }
+        if(position >= sourceSize || byte1 < 0xC0 || (source[position] & 0xC0) != 0x80)
+        {
+            retval += replacementChar;
+            continue;
+        }
+        bool invalid = byte1 < 0xC2 || byte1 > 0xF4;
+        unsigned long byte2 = source[position++];
+        if(byte1 < 0xE0)
+        {
+            if(invalid)
+                retval += replacementChar;
+            else
+                retval += static_cast<char32_t>(((byte1 & 0x1F) << 6) | (byte2 & 0x3F));
+            continue;
+        }
+        if(position >= sourceSize || (source[position] & 0xC0) != 0x80)
+        {
+            retval += replacementChar;
+            continue;
+        }
+        unsigned long byte3 = source[position++];
+        if(byte1 < 0xF0)
+        {
+            if(byte1 == 0xE0 && byte2 < 0xA0)
+                invalid = true;
+            if(invalid)
+                retval += replacementChar;
+            else
+                retval += static_cast<char32_t>(((byte1 & 0xF) << 12) | ((byte2 & 0x3F) << 6)
+                                                | (byte3 & 0x3F));
+            continue;
+        }
+        if(position >= sourceSize || (source[position] & 0xC0) != 0x80)
+        {
+            retval += replacementChar;
+            continue;
+        }
+        unsigned long byte4 = source[position++];
+        if(byte1 == 0xF0 && byte2 < 0x90)
+            invalid = true;
+        if(byte1 == 0xF4 && byte2 > 0x8F)
+            invalid = true;
+        if(byte1 > 0xF4)
+            invalid = true;
+        if(invalid)
+            retval += replacementChar;
+        else
+            retval += static_cast<char32_t>(((byte1 & 0x7) << 18) | ((byte2 & 0x3F) << 12)
+                                            | ((byte3 & 0x3F) << 6) | (byte4 & 0x3F));
+    }
+    return makeSource(std::move(retval));
+}
+
+std::string Parser::parseNewLine()
+{
+    RuleResult result;
+    auto retval = internalParseNewLine(0, result, true);
+    assert(!result.empty());
+    if(result.fail())
+        throw ParseError(errorLocation, errorMessage);
+    return retval;
+}
+
+std::string Parser::internalParseNewLine(std::size_t startLocation__, RuleResult &ruleResultOut__, bool isRequiredForSuccess__)
+{
+    std::string returnValue__{};
+    char32_t ch{};
+    auto &ruleResult__ = this->getResults(startLocation__).resultNewLine;
+    if(!ruleResult__.empty() && (ruleResult__.fail() || !isRequiredForSuccess__))
+    {
+        ruleResultOut__ = ruleResult__;
+        return returnValue__;
+    }
+    if(startLocation__ >= this->sourceSize)
+    {
+        ruleResult__ = this->makeFail(startLocation__, "missing end of line (\'\\r\')", isRequiredForSuccess__);
+    }
+    else if(this->source.get()[startLocation__] == U'\r')
+    {
+        ruleResult__ = this->makeSuccess(startLocation__ + 1, startLocation__ + 1);
+    }
+    else
+    {
+        ruleResult__ = this->makeFail(startLocation__, startLocation__ + 1, "missing end of line (\'\\r\')", isRequiredForSuccess__);
+    }
+    if(ruleResult__.success())
+    {
+        auto savedStartLocation__ = startLocation__;
+        startLocation__ = ruleResult__.location;
+        if(startLocation__ >= this->sourceSize)
+        {
+            ruleResult__ = this->makeFail(startLocation__, "missing end of line (\'\\n\')", isRequiredForSuccess__);
+        }
+        else if(this->source.get()[startLocation__] == U'\n')
+        {
+            ruleResult__ = this->makeSuccess(startLocation__ + 1, startLocation__ + 1);
+        }
+        else
+        {
+            ruleResult__ = this->makeFail(startLocation__, startLocation__ + 1, "missing end of line (\'\\n\')", isRequiredForSuccess__);
+        }
+        startLocation__ = savedStartLocation__;
+    }
+    if(ruleResult__.success())
+    {
+        auto savedStartLocation__ = startLocation__;
+        startLocation__ = ruleResult__.location;
+        {
+#line 32 "parser.peg"
+                         (returnValue__) = "\r\n";
+#line 182 "parser.cpp"
+        }
+        ruleResult__ = this->makeSuccess(startLocation__);
+        startLocation__ = savedStartLocation__;
+    }
+    if(ruleResult__.fail())
+    {
+        Parser::RuleResult lastRuleResult__ = ruleResult__;
+        if(startLocation__ >= this->sourceSize)
+        {
+            ruleResult__ = this->makeFail(startLocation__, "unexpected end of input", isRequiredForSuccess__);
+        }
+        else
+        {
+            bool matches = false;
+            if(this->source.get()[startLocation__] == U'\n')
+            {
+                matches = true;
+            }
+            else if(this->source.get()[startLocation__] == U'\r')
+            {
+                matches = true;
+            }
+            if(matches)
+            {
+                ruleResult__ = this->makeSuccess(startLocation__ + 1, startLocation__ + 1);
+                ch = this->source.get()[startLocation__];
+            }
+            else
+            {
+                ruleResult__ = this->makeFail(startLocation__, startLocation__ + 1, "missing line ending", isRequiredForSuccess__);
+            }
+        }
+        if(ruleResult__.success())
+        {
+            auto savedStartLocation__ = startLocation__;
+            startLocation__ = ruleResult__.location;
+            {
+#line 33 "parser.peg"
+                            (returnValue__) += ch;
+#line 222 "parser.cpp"
+            }
+            ruleResult__ = this->makeSuccess(startLocation__);
+            startLocation__ = savedStartLocation__;
+        }
+        if(ruleResult__.success())
+        {
+            if(lastRuleResult__.endLocation >= ruleResult__.endLocation)
+            {
+                ruleResult__.endLocation = lastRuleResult__.endLocation;
+            }
+        }
+    }
+    ruleResultOut__ = ruleResult__;
+    return returnValue__;
+}
+
+char32_t Parser::parseIfsChar()
+{
+    RuleResult result;
+    auto retval = internalParseIfsChar(0, result, true);
+    assert(!result.empty());
+    if(result.fail())
+        throw ParseError(errorLocation, errorMessage);
+    return retval;
+}
+
+char32_t Parser::internalParseIfsChar(std::size_t startLocation__, RuleResult &ruleResultOut__, bool isRequiredForSuccess__)
+{
+    char32_t returnValue__{};
+    char32_t value{};
+    auto &ruleResult__ = this->getResults(startLocation__).resultIfsChar;
+    if(!ruleResult__.empty() && (ruleResult__.fail() || !isRequiredForSuccess__))
+    {
+        ruleResultOut__ = ruleResult__;
+        return returnValue__;
+    }
+    if(startLocation__ >= this->sourceSize)
+    {
+        ruleResult__ = this->makeFail(startLocation__, "unexpected end of input", isRequiredForSuccess__);
+    }
+    else
+    {
+        bool matches = false;
+        if(!matches)
+        {
+            ruleResult__ = this->makeSuccess(startLocation__ + 1, startLocation__ + 1);
+            value = this->source.get()[startLocation__];
+        }
+        else
+        {
+            ruleResult__ = this->makeFail(startLocation__, startLocation__ + 1, "[] not allowed here", isRequiredForSuccess__);
+        }
+    }
+    if(ruleResult__.success())
+    {
+        auto savedStartLocation__ = startLocation__;
+        startLocation__ = ruleResult__.location;
+        {
+            const char *predicateReturnValue__ = nullptr;
+            {
+#line 36 "parser.peg"
+      
+        if(ifsValue.find(value) == std::u32string::npos)
+            (predicateReturnValue__) = "not an IFS character";
+        (returnValue__) = value;
+    
+#line 289 "parser.cpp"
+            }
+            ruleResult__ = this->makeSuccess(startLocation__);
+            if(predicateReturnValue__ != nullptr)
+                ruleResult__ = this->makeFail(startLocation__, predicateReturnValue__, isRequiredForSuccess__);
+        }
+        startLocation__ = savedStartLocation__;
+    }
+    ruleResultOut__ = ruleResult__;
+    return returnValue__;
+}
+}
+}
--- parser/parser.h
+++ parser/parser.h
@@ -1,3 +1,6 @@
+// automatically generated from parser.peg
+#line 1 "parser.peg"
+              
 /*
  * Copyright 2017 Jacob Lifshay
  *
@@ -13,20 +16,182 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef PARSER_PARSER_H_
-#define PARSER_PARSER_H_
+
+#line 21 "parser.h"
+#ifndef PARSER_H_
+#define PARSER_H_
+
+#include <utility>
+#include <cstddef>
+#include <string>
+#include <memory>
+#include <stdexcept>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <cassert>
+#line 21 "parser.peg"
+             
+#include <string>
+
+#line 38 "parser.h"
 
 namespace quick_shell
 {
 namespace parser
 {
-#error finish
-template <typename ASTBuilder, typename InputStream>
 class Parser final
 {
-#error finish
+    Parser(const Parser &) = delete;
+    Parser &operator=(const Parser &) = delete;
+#line 25 "parser.peg"
+            
+public:
+    std::u32string ifsValue;
+
+#line 53 "parser.h"
+
+private:
+    struct RuleResult final
+    {
+        std::size_t location;
+        std::size_t endLocation;
+        bool isSuccess;
+        constexpr RuleResult() noexcept : location(std::string::npos),
+                                          endLocation(0),
+                                          isSuccess(false)
+        {
+        }
+        constexpr RuleResult(std::size_t location, std::size_t endLocation, bool success) noexcept
+            : location(location),
+              endLocation(endLocation),
+              isSuccess(success)
+        {
+        }
+        constexpr bool empty() const
+        {
+            return location == std::string::npos;
+        }
+        constexpr bool success() const
+        {
+            return !empty() && isSuccess;
+        }
+        constexpr bool fail() const
+        {
+            return !empty() && !isSuccess;
+        }
+    };
+    struct Results final
+    {
+        RuleResult resultNewLine;
+        RuleResult resultIfsChar;
+    };
+    struct ResultsChunk final
+    {
+        static constexpr std::size_t allocated = 0x100;
+        Results values[allocated];
+        std::size_t used = 0;
+    };
+
+public:
+    struct ParseError : public std::runtime_error
+    {
+        std::size_t location;
+        const char *message;
+        static std::string makeWhatString(std::size_t location, const char *message)
+        {
+            std::ostringstream ss;
+            ss << "error at " << location << ": " << message;
+            return ss.str();
+        }
+        ParseError(std::size_t location, const char *message)
+            : runtime_error(makeWhatString(location, message)), location(location), message(message)
+        {
+        }
+    };
+
+private:
+    std::vector<Results *> resultsPointers;
+    std::list<ResultsChunk> resultsChunks;
+    Results eofResults;
+    const std::shared_ptr<const char32_t> source;
+    const std::size_t sourceSize;
+    std::size_t errorLocation = 0;
+    std::size_t errorInputEndLocation = 0;
+    const char *errorMessage = "no error";
+
+private:
+    Results &getResults(std::size_t position)
+    {
+        if(position >= sourceSize)
+            return eofResults;
+        Results *&resultsPointer = resultsPointers[position];
+        if(!resultsPointer)
+        {
+            if(resultsChunks.empty() || resultsChunks.back().used >= ResultsChunk::allocated)
+            {
+                resultsChunks.emplace_back();
+            }
+            resultsPointer = &resultsChunks.back().values[resultsChunks.back().used++];
+        }
+        return *resultsPointer;
+    }
+    RuleResult makeFail(std::size_t location,
+                        std::size_t inputEndLocation,
+                        const char *message,
+                        bool isRequiredForSuccess)
+    {
+        if(isRequiredForSuccess && errorInputEndLocation <= inputEndLocation)
+        {
+            errorLocation = location;
+            errorInputEndLocation = inputEndLocation;
+            errorMessage = message;
+        }
+        return RuleResult(location, inputEndLocation, false);
+    }
+    RuleResult makeFail(std::size_t inputEndLocation,
+                        const char *message,
+                        bool isRequiredForSuccess)
+    {
+        return makeFail(inputEndLocation, inputEndLocation, message, isRequiredForSuccess);
+    }
+    static RuleResult makeSuccess(std::size_t location, std::size_t inputEndLocation)
+    {
+        assert(location != std::string::npos);
+        return RuleResult(location, inputEndLocation, true);
+    }
+    static RuleResult makeSuccess(std::size_t inputEndLocation)
+    {
+        assert(inputEndLocation != std::string::npos);
+        return RuleResult(inputEndLocation, inputEndLocation, true);
+    }
+    static std::pair<std::shared_ptr<const char32_t>, std::size_t> makeSource(
+        std::u32string source);
+    static std::pair<std::shared_ptr<const char32_t>, std::size_t> makeSource(
+        const char *source, std::size_t sourceSize);
+
+public:
+    Parser(std::pair<std::shared_ptr<const char32_t>, std::size_t> source)
+        : Parser(std::move(std::get<0>(source)), std::get<1>(source))
+    {
+    }
+    Parser(std::shared_ptr<const char32_t> source, std::size_t sourceSize);
+    Parser(std::u32string source);
+    Parser(const char *source, std::size_t sourceSize);
+    Parser(const char32_t *source, std::size_t sourceSize);
+    Parser(const std::string &source) : Parser(source.data(), source.size())
+    {
+    }
+
+public:
+    std::string parseNewLine();
+    char32_t parseIfsChar();
+
+private:
+    std::string internalParseNewLine(std::size_t startLocation, RuleResult &ruleResult, bool isRequiredForSuccess);
+    char32_t internalParseIfsChar(std::size_t startLocation, RuleResult &ruleResult, bool isRequiredForSuccess);
 };
 }
 }
 
-#endif /* PARSER_PARSER_H_ */
+#endif /* PARSER_H_ */
